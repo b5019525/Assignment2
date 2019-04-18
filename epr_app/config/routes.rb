341a0b892a6658dbee2ba6be782577d9bca1c75c
b5019525 <b5019525@my.shu.ac.uk>
2019-04-18 11:26:33 +0100

    Created 3 models and controllers

--- epr_app/config/routes.rb
+++ epr_app/config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/tablet'
-  get 'static_pages/tv'
-  get 'static_pages/phone'
   get 'static_pages/home'
   root 'static_pages#home'
   get 'static_pages/about'
   get 'static_pages/contact'
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
