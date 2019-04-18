6d341f1db8dea5d669894e8911b123cf78242a17
b5019525 <b5019525@my.shu.ac.uk>
2019-04-18 15:32:23 +0100

    user can register and create accounts, only registered users can leave reviews

--- epr_app/config/routes.rb
+++ epr_app/config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :iphones
   resources :smarts
   resources :tablets
+  resources :users
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
